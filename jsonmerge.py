ef146793b23bd72b284982b18b426580cd61aff8
bd0786 <38130277+baba-dude@users.noreply.github.com>
2021-11-11 21:43:38 +0530

    Update jsonmerge.py
    Added test Back

--- jsonmerge.py
+++ jsonmerge.py
@@ -1,10 +1,10 @@
 #!/usr/bin/env python
 import glob
 import os
-report_path = "/tmp/*patch_report*.json"
+report_path = "/tmp/test/*patch_report*.json"
 read_files = glob.glob(report_path)
 
-final_report = "/tmp/merged_file.json"
+final_report = "/tmp/test/merged_file.json"
 with open(final_report, "wb") as outfile:
     outfile.write('[{}]'.format(
         ','.join([open(f, "rb").read() for f in read_files])))
